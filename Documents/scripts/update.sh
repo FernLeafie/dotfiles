81903979a194278419219fc4c8407fc4e9bff114
Fernleafie <fernleafie@gmail.com>
2025-10-25 12:00:27 +0200

    update to update script
    yay now clears the cache instead of pacman, ensuring that no yay cache is left aswell as pacman
    yay also removes unused packages

--- Documents/scripts/update.sh
+++ Documents/scripts/update.sh
@@ -4,8 +4,9 @@ sudo pacman -Syu
 yay -Syu
 flatpak update
 cargo install-update -a
-sudo pacman -Scc
+yay -Scc
 sudo pacman -Rns $(pacman -Qdtq)
+yay -Yc
 flatpak uninstall --unused
 notify-send -i emblem-synchronizing-symbolic "Sysups" "Finished system update" -a "Sysups"
 echo Done!
