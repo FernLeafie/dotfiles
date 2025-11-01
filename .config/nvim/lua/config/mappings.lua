2be88f7b81a8478a4faa83e928586adf6b7425ba
Fernleafie <fernleafie@gmail.com>
2025-11-01 17:59:05 +0100

    nvim: better toggle for nvim tree

--- .config/nvim/lua/config/mappings.lua
+++ .config/nvim/lua/config/mappings.lua
@@ -4,5 +4,16 @@ local function map(m, k, v)
 	vim.keymap.set(m, k, v, { noremap = true, silent = true })
 end
 
--- misc
-map("n", "<C-t>", ":NvimTreeToggle<CR>") --open file explorer
+-- nvimtree
+local nvimTreeFocusOrToggle = function ()
+	local nvimTree=require("nvim-tree.api")
+	local currentBuf = vim.api.nvim_get_current_buf()
+	local currentBufFt = vim.api.nvim_get_option_value("filetype", { buf = currentBuf })
+	if currentBufFt == "NvimTree" then
+		nvimTree.tree.toggle()
+	else
+		nvimTree.tree.focus()
+	end
+end
+
+vim.keymap.set("n", "<C-t>", nvimTreeFocusOrToggle) --open or focus file explorer if not open
