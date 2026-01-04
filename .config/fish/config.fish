a1941946bd9323dd706e44dc746cdc0611bd431e
Fernleafie <fernleafie@gmail.com>
2026-01-04 16:25:22 +0100

    [fastfetch] can now select logo through flag

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -45,7 +45,12 @@ if status is-interactive
     end
     
     function fastfetch
-        cat ~/.config/fastfetch/distros.json | jq (string join '' '.' (grep "^NAME=" /etc/os-release | grep "=.*" -o | sed 's/\"//g' | sed 's/=//g' | sed 's/ //g' | sed "s#\/##g") '[]') -r | env fastfetch --file /dev/stdin $argv
+        argparse -i 'd/distro=&' -- $argv
+        if set -ql _flag_d
+            cat ~/.config/fastfetch/distros.json | jq (string join '' '.' $_flag_d '[]') -r | env fastfetch --file /dev/stdin $argv
+        else
+            cat ~/.config/fastfetch/distros.json | jq (string join '' '.' (grep "^NAME=" /etc/os-release | grep "=.*" -o | sed 's/\"//g' | sed 's/=//g' | sed 's/ //g' | sed "s#\/##g") '[]') -r | command fastfetch --file /dev/stdin $argv
+        end
     end
         # equivalent function in bash
         # cat ~/.config/fastfetch/distros.json | jq ".$(grep "^NAME=" /etc/os-release | grep "=.*" -o | sed 's/\"//g' | sed 's/=//g' | sed 's/ //g' | sed "s#\/##g")[]" -r | env fastfetch --file /dev/stdin  $argv
