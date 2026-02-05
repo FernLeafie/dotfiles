b69faaad3088445fb9588cfdf75df7f11394a583
Fernleafie <fernleafie@gmail.com>
2026-02-05 08:08:56 +0100

    [hyfetch] now does custom flags with custom themes
    hyfetch script can now obtain custom flags in custom themes from a json file at ~/.config/hyfetch/flags.json

--- .config/fish/functions/hyfetch.fish
+++ .config/fish/functions/hyfetch.fish
@@ -1,9 +1,12 @@
 function hyfetch
-    argparse -i 'd/distro=&' -- $argv
+    argparse -i 'd/distro=&' 't/theme=&' 'f/flag=&' -- $argv
+    if set -ql _flag_f; and set -ql _flag_t
+        set color (string join '' -- '-p ' (cat ~/.config/hyfetch/flags.json | jq (string join '' '.' $_flag_t '.' $_flag_f '[]')  -r | paste -sd ',')) 
+    end
     if set -ql _flag_d
         set -l _flag_d (echo $_flag_d | tr '[:upper:]' '[:lower:]' | sed "s/linux\$//g; s/gnu\$//g; s/os\$//g;")
-        cat ~/.config/fastfetch/distros.json | jq (string join '' '.' $_flag_d '[]') -r | sed "s/\$.//g" | command hyfetch --ascii-file=/dev/stdin $argv
+        cat ~/.config/fastfetch/distros.json | jq (string join '' '.' $_flag_d '[]') -r | sed "s/\$.//g" | command hyfetch --ascii-file=/dev/stdin $color $argv
     else
-        cat ~/.config/fastfetch/distros.json | jq (string join '' '.' (grep -oP '(?<=^NAME=")[^"]+(?=")' /etc/os-release | tr '[:upper:]' '[:lower:]' | sed 's/[^a-z]//g; s/linux$//g; s/gnu$//g; s/os$//g') '[]') -r | sed "s/\$.//g" | command hyfetch --ascii-file=/dev/stdin $argv
+        cat ~/.config/fastfetch/distros.json | jq (string join '' '.' (grep -oP '(?<=^NAME=")[^"]+(?=")' /etc/os-release | tr '[:upper:]' '[:lower:]' | sed 's/[^a-z]//g; s/linux$//g; s/gnu$//g; s/os$//g') '[]') -r | sed "s/\$.//g" | command hyfetch --ascii-file=/dev/stdin $color $argv
     end
 end
