bc49e120c9b158c684f006df0046eb034b5d8ef2
Fernleafie <fernleafie@gmail.com>
2026-04-01 19:53:15 +0200

    [fish] fixed sysups script issue
    fixes an issue where aur downloads tend to leave an empty download folder in the package cache, that cant be removed by the package cleaning

--- .config/fish/functions/sysups.fish
+++ .config/fish/functions/sysups.fish
@@ -5,6 +5,9 @@ function sysups
   yay -Syu
   flatpak update
   cargo install-update -a
+  if ls /var/cache/pacman/pkg/ | grep -q "^download-.*"
+      sudo rmdir /var/cache/pacman/pkg/download-*
+  end
   yay -Scc
   sudo pacman -Rns $(pacman -Qdtq)
   yay -Yc
