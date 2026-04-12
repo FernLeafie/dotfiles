a4e3f71efe64548413120e03e9e86de45e95929c
Fernleafie <fernleafie@gmail.com>
2026-04-12 19:42:58 +0200

    [nvim] some config modularisation

---
+++ .config/nvim/lua/config/mappings.lua
@@ -0,0 +1,15 @@
+-- creates a custom command for force restart since :restart! isn't a thing
+vim.api.nvim_create_user_command('Restart', '%bdelete! | restart', {desc = 'Force restart'} )
+
+-- General keymaps
+-- Move block
+vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv", { desc = 'Move visual block up' })
+vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv", { desc = 'Move visual block down' })
+
+vim.keymap.set('n', 'J', 'mzJ`z', { desc = 'Append following line' })
+
+-- Paste without overwriting register
+vim.keymap.set('x', '<leader>p', [['_dP]], { desc = 'Paste while perserving clipboard' })
+
+-- Replace word under cursor across the entire buffer
+vim.keymap.set('n', '<leader>s', [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]], { desc = 'Replace word under cursor' })
---
+++ .config/nvim/lua/config/options.lua
@@ -0,0 +1,38 @@
+
+local options = {
+  showmode = false, -- not needed due to lualine
+  clipboard = 'unnamedplus', -- uses the system clipboard
+  ttyfast = true, -- faster scrolling
+  smoothscroll = true,
+  scrolloff = 8,
+
+	number = true, -- numbering lines
+	relativenumber = true,
+	cursorline = true,
+  -- winborder = 'rounded',
+
+	expandtab = true,
+	tabstop = 2,
+	softtabstop = 2,
+	shiftwidth = 2,
+
+	autoindent = true,
+  smartindent = true,
+
+  foldmethod = 'indent',
+  foldenable = true,
+  foldlevel = 99,
+  foldlevelstart = 99,
+  fillchars = 'eob: ,fold: ,foldopen:,foldsep: ,foldinner: ,foldclose:',
+  foldcolumn = '1',
+
+	completeopt = { 'menuone', 'noselect' },
+}
+
+for k, v in pairs(options) do
+	vim.opt[k] = v
+end
+
+vim.diagnostic.config({
+	signs = false,
+})
