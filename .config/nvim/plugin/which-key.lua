93ebec21ed02d9cd4b7b294fb6ffb8fe2773d64e
Fernleafie <fernleafie@gmail.com>
2026-04-17 19:47:56 +0200

    [nvim] whichkey group names

--- .config/nvim/plugin/which-key.lua
+++ .config/nvim/plugin/which-key.lua
@@ -7,3 +7,12 @@ require('which-key').setup({
 vim.keymap.set('n', '<leader>?', function()
 	require('which-key').show({ global = false })
 end, { desc = 'Buffer Local Keymaps (which-key)' })
+
+local wk = require('which-key')
+wk.add({
+	{ '<leader>b', group = 'Buffers' },
+	{ '<leader>c', group = 'Code actions' },
+	{ '<leader>d', group = 'Debug and diagnostics' },
+	{ '<leader>f', group = 'Find files' },
+	{ '<leader>g', group = 'Lsp actions', icon = { icon = '', color = 'green'} },
+})
