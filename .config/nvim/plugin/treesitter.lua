11d37d60f3e3a355608ee7df7b09ccc14c02c909
Fernleafie <fernleafie@gmail.com>
2026-06-04 12:49:57 +0200

    [nvim] updates, and switching out nvim-treesitter for nvim-treesitter-manager

--- .config/nvim/plugin/treesitter.lua
+++ .config/nvim/plugin/treesitter.lua
@@ -1,33 +1,51 @@
-vim.api.nvim_create_autocmd('PackChanged', {
-	callback = function(ev)
-		local name, kind = ev.data.spec.name, ev.data.kind
-		if name == 'nvim-treesitter' and kind == 'update' then
-			if not ev.data.active then
-				vim.cmd.packadd('nvim-treesitter')
-			end
-			vim.cmd('TSUpdate')
-		end
-	end,
-})
+-- vim.api.nvim_create_autocmd('PackChanged', {
+-- 	callback = function(ev)
+-- 		local name, kind = ev.data.spec.name, ev.data.kind
+-- 		if name == 'nvim-treesitter' and kind == 'update' then
+-- 			if not ev.data.active then
+-- 				vim.cmd.packadd('nvim-treesitter')
+-- 			end
+-- 			vim.cmd('TSUpdate')
+-- 		end
+-- 	end,
+-- })
 
 vim.pack.add({
-	'https://github.com/nvim-treesitter/nvim-treesitter',
+	-- 'https://github.com/nvim-treesitter/nvim-treesitter',
 	'https://github.com/nvim-treesitter/nvim-treesitter-context',
+	'https://github.com/romus204/tree-sitter-manager.nvim',
 })
 
-require('nvim-treesitter').install({
-	'bash',
-	'css',
-	'html',
-	'javascript',
-	'json',
-	'kdl',
-	'lua',
-	'markdown',
-	'markdown_inline',
-	'ron',
-	'rust',
+require('tree-sitter-manager').setup({
+	ensure_installed = {
+		'bash',
+		'css',
+		'html',
+		'javascript',
+		'json',
+		'kdl',
+		'lua',
+		'markdown',
+		'markdown_inline',
+		'ron',
+		'rust',
+	},
+	border = 'rounded',
 })
+
+-- require('nvim-treesitter').install({
+-- 	'bash',
+-- 	'css',
+-- 	'html',
+-- 	'javascript',
+-- 	'json',
+-- 	'kdl',
+-- 	'lua',
+-- 	'markdown',
+-- 	'markdown_inline',
+-- 	'ron',
+-- 	'rust',
+-- })
 
 require('treesitter-context').setup({
 	mode = 'topline',
@@ -35,10 +53,10 @@ require('treesitter-context').setup({
 })
 vim.cmd('hi TreesitterContextBottom gui=underdouble guisp=#45475b')
 
-vim.api.nvim_create_autocmd('FileType', {
-	pattern = { '<filetype>' },
-	callback = function()
-		vim.treesitter.start()
-		vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
-	end,
-})
+-- vim.api.nvim_create_autocmd('FileType', {
+-- 	pattern = { '<filetype>' },
+-- 	callback = function()
+-- 		vim.treesitter.start()
+-- 		vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+-- 	end,
+-- })
