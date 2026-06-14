62ae18f228e082da904f74b756f50db7f3751e35
Fernleafie <fernleafie@gmail.com>
2026-06-14 20:19:31 +0200

    [hyfetch] more resilient regex

--- .config/fish/functions/hyfetch.fish
+++ .config/fish/functions/hyfetch.fish
@@ -7,6 +7,6 @@ function hyfetch
         set -l _flag_d (echo $_flag_d | tr '[:upper:]' '[:lower:]' | sed "s/linux\$//g; s/gnu\$//g; s/os\$//g;")
         cat ~/.config/fastfetch/distros.json | jq (string join '' '.' $_flag_d '[]') -r | sed "s/\$.//g" | command hyfetch --ascii-file=/dev/stdin $color $argv
     else
-        cat ~/.config/fastfetch/distros.json | jq (string join '' '.' (grep -oP '(?<=^NAME=")[^"]+(?=")' /etc/os-release | tr '[:upper:]' '[:lower:]' | sed 's/[^a-z]//g; s/linux$//g; s/gnu$//g; s/os$//g') '[]') -r | sed "s/\$.//g" | command hyfetch --ascii-file=/dev/stdin $color $argv
+        cat ~/.config/fastfetch/distros.json | jq (string join '' '.' (grep -oP '(?<=^NAME=(")?)[^"]+(?=(")?$)' /etc/os-release | tr '[:upper:]' '[:lower:]' | sed 's/[^a-z]//g; s/linux$//g; s/gnu$//g; s/os$//g') '[]') -r | sed "s/\$.//g" | command hyfetch --ascii-file=/dev/stdin $color $argv
     end
 end
