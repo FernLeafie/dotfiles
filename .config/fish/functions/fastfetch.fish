d470b6c299c49bd758ab33ab2864cd5f94f8b989
Fernleafie <fernleafie@gmail.com>
2026-06-14 20:19:16 +0200

    [fastfetch] more resilent regex

--- .config/fish/functions/fastfetch.fish
+++ .config/fish/functions/fastfetch.fish
@@ -4,7 +4,7 @@ function fastfetch
         set -l _flag_d (echo $_flag_d | tr '[:upper:]' '[:lower:]' | sed "s/linux\$//g; s/gnu\$//g; s/os\$//g;")
         cat ~/.config/fastfetch/distros.json | jq (string join '' '.' $_flag_d '[]') -r | env fastfetch --file /dev/stdin $argv
     else
-        cat ~/.config/fastfetch/distros.json | jq (string join '' '.' (grep -oP '(?<=^NAME=")[^"]+(?=")' /etc/os-release | tr '[:upper:]' '[:lower:]' | sed 's/[^a-z]//g; s/linux$//g; s/gnu$//g; s/os$//g') '[]') -r | command fastfetch --file /dev/stdin $argv
+        cat ~/.config/fastfetch/distros.json | jq (string join '' '.' (grep -oP '(?<=^NAME=(")?)[^"]+(?=(")?$)' /etc/os-release | tr '[:upper:]' '[:lower:]' | sed 's/[^a-z]//g; s/linux$//g; s/gnu$//g; s/os$//g') '[]') -r | command fastfetch --file /dev/stdin $argv
     end
 end
 
